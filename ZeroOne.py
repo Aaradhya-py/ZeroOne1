430759c02711d5433940ff09bf0f4bf75341a382
Aaradhya-py <manish02006@gmail.com>
2025-09-07 15:20:21 +0530

    Add files via upload

---
+++ ZeroOne.py
@@ -0,0 +1,43 @@
+import streamlit as st
+st.markdown(
+    """
+    <style>
+    .big-heading {
+        font-family: 'Consolas', 'Courier New', monospace;
+        font-size: 40px;
+        font-weight;
+        text-align: left;
+        color: black;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Then replace st.title with this:
+st.markdown("<div class='big-heading'>ZeroOne</div>", unsafe_allow_html=True)
+def text_to_binary(message: str) -> str:
+    return ' '.join(format(ord(i), '08b') for i in message)
+
+def binary_to_text(binary: str) -> str:
+    try:
+        return ''.join([chr(int(b, 2)) for b in binary.split()])
+    except Exception:
+        return None
+
+st.caption('''Welcome to ZeroOne, This app converts your any message to binary and any binary to text.
+           Give it a try: ''')
+
+user_input = st.text_area("Enter text or binary")
+
+if st.button("Convert"):
+    if all(ch in "01 " for ch in user_input.strip()):
+        result = binary_to_text(user_input)
+        if result is None:
+            st.error("Invalid binary input!")
+        else:
+            st.success(f"Text: {result}")
+    else:
+        result = text_to_binary(user_input)
+        st.success(f"Binary: {result}")
+st.markdown("---")
---
+++ readme.md
@@ -0,0 +1,12 @@
+Hey Guys! how's it going?
+My name is Aaradhya Rampuriya
+
+I made my first app today named "ZeroOne1" 
+hope you like it.
+
+It took so much time but finally it is here.
+Make sure to share it and use it as much as you like.
+For Feedbacks: 
+Check my Instagram: Otaku_artist_aaradhya
+
+Thank You!
